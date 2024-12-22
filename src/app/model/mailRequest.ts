3cdb3d917d7bf0b211258f2e84be9f1d8b4cb5e0
Yanan LYU <arol666@hotmail.com>
2024-12-22 15:21:19 +0100

    chore: format prettier

--- src/app/model/mailRequest.ts
+++ src/app/model/mailRequest.ts
@@ -1,15 +1,15 @@
 export interface MailRequest {
-    name: string,
-    from: string,
-    to: string,
-    value: number
+  name: string
+  from: string
+  to: string
+  value: number
 }
 
 export interface MailJetRequest {
-    FromEmail: string,
-    FromName: string,
-    Subject: string,
-    'Text-part': string,
-    'Html-part': string,
-    Recipients: { Email: string }[]
+  FromEmail: string
+  FromName: string
+  Subject: string
+  'Text-part': string
+  'Html-part': string
+  Recipients: { Email: string }[]
 }
--- src/app/services/firebase.ts
+++ src/app/services/firebase.ts
@@ -1,14 +1,14 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDAQMc8WSn1IVVYg7_u-84u0QShlvf1Ask",
-    authDomain: "secret-santa-f6665.firebaseapp.com",
-    projectId: "secret-santa-f6665",
-    storageBucket: "secret-santa-f6665.firebasestorage.app",
-    messagingSenderId: "473580335234",
-    appId: "1:473580335234:web:1dbbbeef0bfd71711e049b",
-    measurementId: "G-9CMLNH15ZF"
-  };
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+  apiKey: 'AIzaSyDAQMc8WSn1IVVYg7_u-84u0QShlvf1Ask',
+  authDomain: 'secret-santa-f6665.firebaseapp.com',
+  projectId: 'secret-santa-f6665',
+  storageBucket: 'secret-santa-f6665.firebasestorage.app',
+  messagingSenderId: '473580335234',
+  appId: '1:473580335234:web:1dbbbeef0bfd71711e049b',
+  measurementId: 'G-9CMLNH15ZF',
+}
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
