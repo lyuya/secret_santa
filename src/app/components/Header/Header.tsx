efb4bd4725c128c6fa7f47dff92a2c06cd15a19a
Yanan LYU <arol666@hotmail.com>
2024-12-25 20:59:54 +0100

    feat: init user info in header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,16 +1,18 @@
 'use client'
 
-import { logout } from '@/app/services/auth.service'
+import { login, logout } from '@/app/services/auth.service'
 import { auth } from '@/app/services/firebase'
-import { GoogleAuthProvider, signInWithPopup, User } from 'firebase/auth'
+import { User } from 'firebase/auth'
 import { useEffect, useState } from 'react'
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
 import LogoutIcon from '@mui/icons-material/Logout';
 export default function Header() {
-  const [user, setUser] = useState<User>()
+  const [user, setUser] = useState<User | null>()
   const connect = async () => {
-    const provider = new GoogleAuthProvider();
-    await signInWithPopup(auth, provider);  }
+    await login()
+    const user = auth.currentUser
+    if (user) setUser(user)
+  }
 
   const disconnect = async () => {
     await logout()
@@ -18,21 +20,24 @@ export default function Header() {
   }
 
   useEffect(() => {
-    const _user = auth.currentUser
-    console.log('_user', _user)
-    if (_user) setUser(_user)
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
   }, [])
   return (
     <header className="float-right p-5">
       {user && (
         <>
-            <span>Hi ! {user.displayName}</span>
-            <button
-                className="text-red-900 hover:text-red-700"
-                onClick={() => disconnect()}
-            >
-                <LogoutIcon></LogoutIcon>
-            </button>
+            <div>Hi ! {user.displayName}</div>
+            <div className='justify-self-center'>
+              <button
+                  className="text-red-900 hover:text-red-700"
+                  onClick={() => disconnect()}
+              >
+                  <LogoutIcon></LogoutIcon>
+              </button>
+            </div>
+
         </>
         )}
       {!user && (
